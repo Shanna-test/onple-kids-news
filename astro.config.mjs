bca619e6f56c8ad28eaca43416ffdc0678211106
Shanna-test <storyfactory11@gmail.com>
2025-12-15 21:23:40 +0900

    fix site url for wiggnews.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
-  site: 'https://example.com',
+  site: 'https://wiggnews.com',
+  base: '/',
 });
-
